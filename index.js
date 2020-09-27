3830660823e9917e704cf1b2e95c5cba72ee2082
Johnny123122 <66916966+Johnny123122@users.noreply.github.com>
2020-09-27 17:53:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1125,27 +1125,27 @@ case 'restart':
     break
     case 'ban':
       if(!message.member.hasPermission("BAN_MEMBERS")) {
-        return message.channel.send(`**${message.author.username}**, You do not have perms to ban someone`)
+        return message.reply(`You do not have perms to ban someone`)
       }
       
       if(!message.guild.me.hasPermission("BAN_MEMBERS")) {
-        return message.channel.send(`**${message.author.username}**, I do not have perms to ban someone`)
+        return message.reply(`**I do not have permission to ban anyone`)
       }
       
       const target = message.mentions.members.first();
       
       if(!target) {
-        return message.channel.send(`**${message.author.username}**, Please mention the person who you want to ban.`)
+        return message.reply(`Please mention the person who you want to ban.`)
       }
       
       if(target.id === message.author.id) {
-        return message.channel.send(`**${message.author.username}**, You can not ban yourself!`)
+        return message.reply(`You can not ban yourself!`)
       }
       
      
       
      if(!args[1]) {
-       return message.channel.send(`**${message.author.username}**, Please Give Reason To ban Member`)
+       return message.reply(`Please Give Reason To ban Member`)
      }
       
       let embed1212121212121212 = new MessageEmbed()
