36b71add71d4f3b59dc4a1d1c2f8029f4a7c21c7
Johnny123122 <66916966+Johnny123122@users.noreply.github.com>
2020-09-27 18:04:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1157,6 +1157,20 @@ case 'restart':
       message.channel.send(embed1212121212121212)
   message.guild.members.ban(target, { reason: reason121212121121 }).catch(err => console.log(err));    
   break
+  case 'unban':
+    if (!message.member.hasPermission(`BAN_MEMBERS`)) return message.reply(`You can't use this command!`)
+    let userID = args[0]
+      message.guild.fetchBans().then(bans=> {
+      if(bans.size == 0) return 
+      let bUser = bans.find(b => b.user.id == userID)
+      if(!bUser) return message.reply(`The User ID you entered isn't valid!`)
+      message.guild.members.unban(bUser.user)
+      const unbanconfirmation = new MessageEmbed()
+      .setTitle(`${client.users.cache.get(userID).tag} has sucessfully been unbanned!`)
+      .setColor('GREEN')
+      message.channel.send(unbanconfirmation)
+      })
+      break
       case 'cal':
         const math = require('discord-math');
         let num1 = Number(args[0]);
