b5977ba05d13ad447e4f04101971ee37e5f867cb
Johnny123122 <66916966+Johnny123122@users.noreply.github.com>
2020-09-27 15:16:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -841,7 +841,47 @@ case '8ball':
         .setColor(`RANDOM`)
       message.channel.send(Embed11)
     }
-   break 
+   break
+   case 'roblox':
+    const roblox = require("noblox.js")
+     if (username) {
+   roblox.getIdFromUsername(username).then(id => {
+     
+     // if an identity is found under the username then continue collecting the rest of the data
+     // sadly this means you can't search for banned users. f in the chat. maybe try using older apis
+     // yes, i just did c# styled bracketing, do not mind me trying to bless your eyes
+     
+     if (id) 
+
+     {
+       // next conditio
+       roblox.getPlayerInfo(parseInt(id)).then(function(info) 
+
+       {
+         // dates.. um. go try get a pear or a grape instead.
+         let date = new Date(info.joinDate)
+         let dateInfo = client.extractDate(date)
+         
+         // create new embed and establish some settings for it, tasty.
+         let embed676767 = new MessageEmbed()
+         .setTitle(`${username}'s Profile`)
+         .setColor('RANDOM')
+         .setThumbnail(`https://www.roblox.com/bust-thumbnail/image?userId=${id}&width=420&height=420&format=png`)
+         .addField("Username", info.username || 'Unresolvable', true)
+         .addField("User ID", id || 'Unresolvable', true)
+         .addField("Description", info.blurb || 'Nothing', true)
+         .addField("Status", info.status || 'Nothing', true)
+         .addField("Account Age", `${info.age} days old` || 'Unresolvable')
+         .addField("User Link", `https://roblox.com/users/${id}/profile`)
+         .setFooter(`Requested by: ${message.author.tag}`,message.author.displayAvatarURL({ dynamic: true }))
+         message.channel.send(embed676767)
+       })
+     }
+   }).catch(function (err) {
+    message.channel.send("That user doesn't exist, perhaps you misspelled? Double check your spelling!") // catching error
+  });  
+} else { message.channel.send("Missing the user argument!") }
+break 
    case 'covid':
      const Discord = require('discord.js')
     const fetch = require('node-fetch');
