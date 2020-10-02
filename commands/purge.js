033eca9586efd66d85ea7a224e6dee087744decd
Johnny123122 <66916966+Johnny123122@users.noreply.github.com>
2020-10-02 10:48:45 -0400

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -31,10 +31,9 @@ module.exports.run = async (bot, message, args) => {
       )
       .setFooter(
         `Purge Conducted by: ${message.author.id}`,
-        message.author.displayAvatarURL({ dynamic: true })
+        message.author.displayAvatarURL({ dynamic: true }))
         .setColor(
           'GREEN'
-        )
       );
     message.channel.send({ embed: embed }).then(m => m.delete({ timeout: 5000 }))
   });
